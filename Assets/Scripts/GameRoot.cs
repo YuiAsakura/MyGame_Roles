3e071c64e3365abc49f51e6d9ba5d1645d93390a
YuiAsakura <sirokogu333@gmail.com>
2025-09-21 02:28:35 +0900

    [add] #8 Singleton

--- Assets/Scripts/GameRoot.cs
+++ Assets/Scripts/GameRoot.cs
@@ -1,7 +1,22 @@
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameRoot : MonoBehaviour
 {
+    public static GameRoot Instance { get; private set;}
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            // 自身をインスタンスとする
+            Instance = this;
+        }
+        else
+        {
+            // インスタンスが複数存在しないように、既に存在していたら自身を消去する
+            Destroy(GameRoot);
+        }
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {

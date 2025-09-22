ebf75caa768aefbc61e4ce3a49f187922b8940ea
YuiAsakura <sirokogu333@gmail.com>
2025-09-22 23:53:23 +0900

    [add] #17 add PersonalSystem.cs

--- Assets/Scripts/PersonalSystem.cs
+++ Assets/Scripts/PersonalSystem.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PersonalSystem : MonoBehaviour
 {
+    private TimeCounter timeCounter;
+
     // 個性値
     public int seeking;     // 探求力
     public int decision;    // 決断力
@@ -9,9 +11,17 @@ public class PersonalSystem : MonoBehaviour
     public int patience;    // 忍耐力
     public int insight;     // 洞察力
 
-    void Start()
+    private void Start()
     {
         Initialize();
+
+        // シーン内のTimeCounterコンポーネントを探して取得する
+        timeCounter = FindObjectOfType<TimeCounter>();
+        if (timeCounter != null)
+        {
+            // TimeCounterのOnTimeUpイベントにSeekRole()メソッドを登録する
+            timeCounter.OnTimeUp += SeekRole;
+        }
     }
 
     void Update()
@@ -23,5 +33,11 @@ public class PersonalSystem : MonoBehaviour
     {
         // 個性値を初期化
         seeking = 0; decision = 0; sensitive = 0; patience = 0; insight = 0;
+    }
+
+    private void SeekRole()
+    {
+        Debug.Log("Set Role");
+        GameRoot.I.ThisRole = "Player";
     }
 }

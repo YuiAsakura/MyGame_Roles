f903e548b4efe9fa47126db6865b0bb0afe25ba8
YuiAsakura <sirokogu333@gmail.com>
2025-09-22 18:52:12 +0900

    [fix] fix code GameRoot.cs

--- Assets/Scripts/GameRoot.cs
+++ Assets/Scripts/GameRoot.cs
@@ -3,20 +3,13 @@ using UnityEngine;
 
 public class GameRoot : MonoBehaviour
 {
-    public static GameRoot Instance { get; private set;}
+    public static GameRoot I { get; private set;}
 
     private void Awake()
     {
-        if (Instance == null)
-        {
-            // 自身をインスタンスとする
-            Instance = this;
-        }
-        else
-        {
-            // インスタンスが複数存在しないように、既に存在していたら自身を消去する
-            Destroy(gameObject);
-        }
+        if (I != null && I != this) { Destroy(gameObject); return; }
+        I = this;
+        DontDestroyOnLoad(gameObject);  // シーンをまたいで生存
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()

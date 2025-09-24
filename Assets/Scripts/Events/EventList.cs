19d4da6600d8c359078651a0067038241d00abd6
YuiAsakura <sirokogu333@gmail.com>
2025-09-24 10:07:54 +0900

    [add] create event 01

--- Assets/Scripts/Events/EventList.cs
+++ Assets/Scripts/Events/EventList.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
 
+// Event一覧を管理するスクリプト
+// EventLaunchをアタッチした各オブジェクトに設定した関数が呼び出される
 public class EventList : MonoBehaviour
 {
+    public MessageSystem messageSystem;
+
+    private string[] eventMassage;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void ev01bench()
+    {
+        Debug.Log("event 01 bench");
+        eventMassage = new string[] {
+            "ベンチで休みますか？\n時間を消費します",
+            "OK",
+        };
+        messageSystem.ShowMessages(eventMassage);
     }
 }

f5febe7a856b302912f5c3e83e5b5d507e431e75
YuiAsakura <sirokogu333@gmail.com>
2025-09-30 13:37:01 +0900

    [add] #21 add process that role status

--- Assets/Scripts/GameRoot.cs
+++ Assets/Scripts/GameRoot.cs
@@ -7,6 +7,8 @@ public class GameRoot
     public static GameRoot I { get; private set; } = new GameRoot();
     //public static GameRoot I { get; private set; }
 
+    public float currentTime;
+
     // 状態管理用のbool変数
     public bool isMessage;  // メッセージ表示中
     public bool isEvent;    // イベント実行中
@@ -15,9 +17,14 @@ public class GameRoot
     public bool isActive_Move;      // 移動可能
     public bool isActive_MWindow;   // Message Windowおよび選択
 
-    public string ThisRole;     // 診断した役職の結果
+    // イベント管理用の変数
     public bool selected;       // yes or no
     public int nowLabel;        // イベント進行管理
+
+    // 役職管理用の変数
+    public string ThisRole;     // 診断した役職の結果
+    public int seeking = 0, decision = 0, sensitive = 0, patience = 0, insight = 0;
+
 
     //private void Awake()
     //{
